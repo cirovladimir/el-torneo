9c5fe3b68124cd4a5f1f65f540c179d463b23a71
cirovladimir <cirovladimir@gmail.com>
2012-11-08 23:33:05 -0600

    Added setters for Player properties and default constructor.

--- eltorneo-gwt/src/main/java/mx/com/apestudio/gwt/eltorneo/server/modelo/Jugador.java
+++ eltorneo-gwt/src/main/java/mx/com/apestudio/gwt/eltorneo/server/modelo/Jugador.java
@@ -1,3 +1,4 @@
+/* Copyright (c) 2012, Ciro Vladimir Arreola Camacho. */
 package mx.com.apestudio.gwt.eltorneo.server.modelo;
 
 import java.util.Date;
@@ -6,6 +7,11 @@ import javax.persistence.Id;
 
 import com.google.appengine.api.datastore.Blob;
 
+/**
+ * A class to represent the entity Player
+ * @author cirovladimir
+ *
+ */
 public class Jugador {
 
 	@Id Long id;
@@ -16,4 +22,77 @@ public class Jugador {
 	Float peso;
 	String email;
 	Blob foto;
+	
+	public Jugador() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public Float getAltura() {
+		return altura;
+	}
+
+	public void setAltura(Float altura) {
+		this.altura = altura;
+	}
+
+	public Float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Float peso) {
+		this.peso = peso;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Blob getFoto() {
+		return foto;
+	}
+
+	public void setFoto(Blob foto) {
+		this.foto = foto;
+	}
+	
+	public void setFoto(byte[] foto) {
+		this.foto = new Blob(foto);
+	}
+	
 }
